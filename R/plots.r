13f8cef5190777e9daa148fbf92bc7db7bda0030
Jason Gantenberg <jason.gantenberg@gmail.com>
2024-07-30 14:45:58 -0400

    fix plot margin

--- R/plots.r
+++ R/plots.r
@@ -10,5 +10,5 @@ theme_gpander <- function(...) {
   theme_pander(...) +
     theme(axis.title.x = element_text(margin = margin(t = 0.5, unit = "cm")),
           axis.title.y = element_text(margin = margin(r = 0.5, unit = "cm")),
-          margin = margin(t = 0.5, r = 0.5, b = 0.5, l = 0.5, "cm"))
+          plot.margin = margin(t = 0.5, r = 0.5, b = 0.5, l = 0.5, "cm"))
 }
